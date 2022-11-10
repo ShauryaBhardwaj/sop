b333505535aea0e7a30ddd40565715fddc463da4
Shaurya Bhardwaj <80877612+ShauryaBhardwaj@users.noreply.github.com>
2022-11-10 22:56:27 +0530

    Add files via upload

---
+++ MLX90614_demo.py
@@ -0,0 +1,9 @@
+//MLX90614_demo.py
+
+from smbus2 import SMBus
+from mlx90614 import MLX90614
+bus = SMBus(1)
+sensor = MLX90614(bus, address=0x5A)
+print "Ambient Temperature :", sensor.get_ambient()
+print "Object Temperature :", sensor.get_object_1()
+bus.close()
